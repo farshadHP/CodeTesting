9a10790fd84d20b37d0fc76ad4754e6d42008030
= <=>
2021-12-11 19:26:32 +0330

    some coding in Test class

--- app/src/main/java/com/farshadhp/codetesting/ViewModelTester.kt
+++ app/src/main/java/com/farshadhp/codetesting/ViewModelTester.kt
@@ -3,9 +3,23 @@ package com.farshadhp.codetesting
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 
+import org.junit.Assert
+import java.util.*
+
 class ViewModelTester (private var view : ViewModelStoreOwner){
     private var viewModel : MyViewModel = ViewModelProvider(view).get(MyViewModel::class.java)
+    private var localesArr = Locale.getAvailableLocales()
 
-    
+    fun getDateTime_NetherslandsLocale(){
+        Locale.setDefault(localesArr[516]/*its Netherlands*/)
+        var date = viewModel.getDateTime()
+        Assert.assertEquals("11/12/2021",date)
+    }
+    fun getDateTime_UsLocale(){
+        Locale.setDefault(Locale.US)
+    }
+    fun getDateTime_UkLocale(){
+        Locale.setDefault(Locale.UK)
+    }
 
 }
