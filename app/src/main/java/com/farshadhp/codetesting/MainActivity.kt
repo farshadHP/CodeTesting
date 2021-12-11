db7d4f559a718253d4947022ac9ddbdd39681382
= <=>
2021-12-12 01:51:00 +0330

    remove wrong test class and use testFolder

--- app/src/main/java/com/farshadhp/codetesting/MainActivity.kt
+++ app/src/main/java/com/farshadhp/codetesting/MainActivity.kt
@@ -7,15 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var vTest = ViewModelTester(this)
-        vTest.getDateTime_NetherslandsLocale()
-        vTest.getDateTime_UsLocale()
-        vTest.getDateTime_UkLocale()
-        vTest.getCurrency_NetherslandsLocale()
-        vTest.getCurrency_UsLocale()
-        vTest.getCurrency_UkLocale()
-        vTest.getFloatNum_NetherslandsLocale()
-        vTest.getFloatNum_UsLocale()
-        vTest.getFloatNum_UkLocale()
+        
     }
 }
--- app/src/test/java/com/farshadhp/codetesting/ExampleUnitTest.kt
+++ app/src/test/java/com/farshadhp/codetesting/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.farshadhp.codetesting
 
+
+import org.junit.Assert
 import org.junit.Test
 
-import org.junit.Assert.*
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,67 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    private var viewModel = MyViewModel()
+    private var localesArr = Locale.getAvailableLocales()
+
+    // functions for Date and Time
+
+    @Test    fun getDateTime_NetherslandsLocale(){
+        Locale.setDefault(localesArr[516]/*its Netherlands*/)
+        var date = viewModel.getDateTime()
+        Assert.assertEquals("niedziela, 12 grudnia 2021",date)
     }
+    @Test
+    fun getDateTime_UsLocale(){
+        Locale.setDefault(Locale.US)
+        var date = viewModel.getDateTime()
+        Assert.assertEquals("Sunday, December 12, 2021",date)
+    }
+    @Test
+    fun getDateTime_UkLocale(){
+        Locale.setDefault(Locale.UK)
+        var date = viewModel.getDateTime()
+        Assert.assertEquals("Sunday, 12 December 2021",date)
+    }
+    // functions for Currency
+    @Test
+    fun getCurrency_NetherslandsLocale(){
+        Locale.setDefault(localesArr[516]/*its Netherlands*/)
+        var currency = viewModel.getCurrency()
+        Assert.assertEquals("500 000,00 ¤",currency)
+    }
+    @Test
+    fun getCurrency_UsLocale(){
+        Locale.setDefault(Locale.US)
+        var currency = viewModel.getCurrency()
+        Assert.assertEquals("\$500,000.00",currency)
+    }
+    @Test
+    fun getCurrency_UkLocale(){
+        Locale.setDefault(Locale.UK)
+        var currency = viewModel.getCurrency()
+        Assert.assertEquals("£500,000.00",currency)
+    }
+    // functions for the Float Num
+    @Test
+    fun getFloatNum_NetherslandsLocale(){
+        Locale.setDefault(localesArr[516]/*its Netherlands*/)
+        var num = viewModel.getFloatNum()
+        Assert.assertEquals("3,145",num)
+    }
+    @Test
+    fun getFloatNum_UsLocale(){
+        Locale.setDefault(Locale.US)
+        var num = viewModel.getFloatNum()
+        Assert.assertEquals("3.145",num)
+    }
+    @Test
+    fun getFloatNum_UkLocale(){
+        Locale.setDefault(Locale.UK)
+        var num = viewModel.getFloatNum()
+        Assert.assertEquals("3.145",num)
+    }
+
+
 }
